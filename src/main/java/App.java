d3c4faa7678ee801c605a05e1b621d0e23c176cc
lee <injin33@naver.com>
2018-10-05 16:25:14 +0900

    정정;;

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,10 +5,6 @@ public class App {
         System.out.println("Everybody Isolating in microcosm");
         System.out.println("포로리야~~");
         System.out.println("때릴꼬야?");
-        System.out.println("떄릴고다!!"); 
-        System.out.println("나는 인진");
-        System.out.println("내 밑으로 다");
-        System.out.println("조용히해라");
-        System.out.println("얻어터지기전에");
+        System.out.println("떄릴고다!!");
     }
 }
