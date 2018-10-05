466d21c33f9e5df83e4b703a8bc82f340f32610b
choi <sjchoi0408@hanmail.net>
2018-10-05 15:56:01 +0900

    앱 변경2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,7 +7,7 @@ public class App {
     String message = "ㅓㅏㅁ니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니닐ㅇ";
     
     public String getGreeting() {
-        return "Hiiiiiiiiiiiiiiiiiiiii!.";
+        return "Hiiiiiiiiiiiii!.";
     }
 
 
