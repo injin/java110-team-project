9acf83ae15a67af4af773365234c9df39b514a7e
choi <sjchoi0408@hanmail.net>
2018-10-05 15:28:58 +0900

    앱 변경

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,7 @@
  */
 public class App {
     public String getGreeting() {
-        return "Hello world.";
+        return "Hiiiiiiiiiiiiiiiiiiiii!.";
     }
 
     public static void main(String[] args) {
