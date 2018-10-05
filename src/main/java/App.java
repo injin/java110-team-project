0d4d7578b03be8f1cbcd1b8edc621b4b08a8a1ff
lee <injin33@naver.com>
2018-10-05 15:29:07 +0900

    App 변경

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,10 @@ public class App {
     public String getGreeting() {
         return "Hello world.";
     }
+    
+    public String greetingTo(String name) {
+        return "Hello " + name;
+    }
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
