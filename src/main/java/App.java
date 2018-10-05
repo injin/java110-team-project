c184851bf122238e0722032f2f6502d3e6769e17
kim <ilos1212@naver.com>
2018-10-05 16:12:36 +0900

    ㅇㅇ

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> branch 'master' of https://github.com/injin/java110-team-project.git
 public class App {
-   
+    String message = "ㅓㅏㅁ니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니니닐ㅇ";
     
     public static void main(String[] args) {
-<<<<<<< HEAD
-       System.out.println("요를레히요를레히요를레히히 요를레히이이이~");
-       
-=======
         System.out.println("Everybody Isolating in microcosm");
         System.out.println("포로리야~~");
         System.out.println("때릴꼬야?");
         System.out.println("떄릴고다!!"); 
->>>>>>> branch 'master' of https://github.com/injin/java110-team-project.git
     }
 }
