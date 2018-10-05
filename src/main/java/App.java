55cce2d368ccd3a0ba82f83cb864750e4a60bc84
lee <injin33@naver.com>
2018-10-05 16:20:53 +0900

    ㅊㅊ

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,5 +6,9 @@ public class App {
         System.out.println("포로리야~~");
         System.out.println("때릴꼬야?");
         System.out.println("떄릴고다!!"); 
+        System.out.println("나는 인진");
+        System.out.println("내 밑으로 다");
+        System.out.println("조용히해라");
+        System.out.println("얻어터지기전에");
     }
 }
