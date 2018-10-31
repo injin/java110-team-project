3e323bac59e099ea4962cd3f212c1dc79f031dd1
bit <minsung2da@naver.com>
2018-10-31 09:32:43 +0900

    업로드 테스트

---
+++ src/test/java/UploadTest_minsung.java
@@ -0,0 +1,13 @@
+
+public class UploadTest_minsung {
+
+    
+    public static void main(String[] args) {
+        
+        
+        
+    System.out.println("암더코리안탑클래스힙합모범노블레스페뷸러스터뷸러스골저스벗댕저러스비트를비틀어제껴버리는서브미션챔피언!");
+    }
+    
+    
+}
