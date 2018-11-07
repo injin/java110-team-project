4156f8d29e7cb53cf044c71496f4eea8af033534
kim <ilos1212@naver.com>
2018-11-07 15:53:08 +0900

    new file:   src/main/java/bitcamp/java109/cms/dao/GenreDao.java new file:   src/main/java/bitcamp/java110/cms/domain/Genre.java deleted:    src/main/webapp/HSJE/SignUpDetail.html

---
+++ src/main/java/bitcamp/java110/cms/service/GenreService.java
@@ -0,0 +1,11 @@
+package bitcamp.java110.cms.service;
+
+import java.util.List;
+import bitcamp.java110.cms.domain.Genre;
+
+public interface GenreService {
+  List<Genre> getList();
+//  Genre getByNo(int no);
+//  Genre getByName(String name);
+
+}
