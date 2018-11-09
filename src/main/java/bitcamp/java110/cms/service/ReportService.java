0768664e8457fbbef68008f054bca4c247a7b58d
choi <sjchoi0408@hanmail.net>
2018-11-09 21:06:16 +0900

    신고_sql 문제 해결 필요

---
+++ src/main/java/bitcamp/java110/cms/service/ReportService.java
@@ -0,0 +1,12 @@
+package bitcamp.java110.cms.service;
+
+import java.util.List;
+import bitcamp.java110.cms.domain.Report;
+
+public interface ReportService {
+  
+    void add (Report report);
+    List<Report> list(int pageNo, int pageSize);
+    Report get(int no);
+    void delete(int no);
+}
