6c031beb3ec24b01f708debae4c7b283b0535fb2
choi <sjchoi0408@hanmail.net>
2018-11-13 12:05:33 +0900

    .

--- src/main/java/bitcamp/java110/cms/dao/PostHashtagDao.java
+++ src/main/java/bitcamp/java110/cms/dao/PostHashtagDao.java
@@ -1,9 +1,12 @@
 package bitcamp.java110.cms.dao;
 
+import java.util.List;
 import java.util.Map;
+import bitcamp.java110.cms.domain.Post;
 
 public interface PostHashtagDao {
   
   int insert(Map<String, Object> params);
+  List<Post> get(String keyword);
   int delete(int no);
 }
