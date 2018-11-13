e49ff5a910d31fb43c0d26ea200d49ae6f7e4b49
choi <sjchoi0408@hanmail.net>
2018-11-13 09:29:09 +0900

    검색영화ui

--- src/main/java/bitcamp/java110/cms/dao/MemberDao2.java
+++ src/main/java/bitcamp/java110/cms/dao/MemberDao2.java
@@ -1,10 +1,13 @@
 package bitcamp.java110.cms.dao;
 
+import java.util.List;
 import bitcamp.java110.cms.domain.Member;
 
-public interface MemberDao {
+public interface MemberDao2 {
 
   int insert(Member member);
   Member findById(String id);
+  List<Member> findByNick (String nick);
   void update(Member member);
+  
 }
--- src/main/java/bitcamp/java110/cms/service/MemberService2.java
+++ src/main/java/bitcamp/java110/cms/service/MemberService2.java
@@ -1,9 +1,11 @@
 package bitcamp.java110.cms.service;
 
+import java.util.List;
 import bitcamp.java110.cms.domain.Member;
 
-public interface MemberService {
+public interface MemberService2 {
   void add (Member member);
   void update (Member member);
   Member findById (String id);
+  List<Member> findByNick (String nick);
 }
