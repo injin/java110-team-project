2a8bf8ee0e1770d6d87b305fbc46678cf4e1174d
JeahaOh <jeaha1217@gmail.com>
2018-11-14 09:15:53 +0900

    주석 추가

--- src/main/java/bitcamp/java110/cms/web/auth/AuthController.java
+++ src/main/java/bitcamp/java110/cms/web/auth/AuthController.java
@@ -73,8 +73,9 @@ public class AuthController {
     
     
     /**
+     * !!주석 삭제 금지!!
      * 제하 To Do
-     * kakao 연동 해제 NOTICE 주석 삭제 금지
+     * kakao 연동 해제 NOTICE
      * !! SginOut 할 때 DB에서 삭제 해야하므로 자주 UPDATE 해야함!!
      */
     @RequestMapping("/signOut")
