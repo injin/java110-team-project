b64f05a31df1a651cd12f1d2ad454b481262aef5
bit <minsung2da@naver.com>
2018-11-17 13:41:19 +0900

    package

---
+++ src/main/java/bitcamp/java110/cms/web/follow/FlwController.java
@@ -0,0 +1,80 @@
+package bitcamp.java110.cms.web.follow;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import bitcamp.java110.cms.dao.FlwDao;
+import bitcamp.java110.cms.domain.Flw;
+import bitcamp.java110.cms.domain.Member;
+import bitcamp.java110.cms.service.FlwService;
+
+@Controller
+@RequestMapping("/flw")
+public class FlwController {
+
+
+    @Autowired
+    FlwService flwService;
+
+    @Autowired 
+    FlwDao flwDao;
+
+    @RequestMapping("flwlist")
+    public String flwlist(Model model,
+            HttpSession session) {
+
+        Member member = (Member) session.getAttribute("loginUser");
+        List<Flw> a = flwService.list(member.getMno());
+        model.addAttribute("flwlist" , a);
+
+        return "follow/flwlist";
+
+    }
+
+ 
+
+
+    /*    @RequestMapping("list")
+    public String list(
+              Model model //Model객체 생성
+                    ) {
+
+        //flwService에 있는 list메서드를 사용해서
+        //List<Ms>의 객체 a에 담는다.
+        List<Flw> a = flwService.list();
+
+        //Model addAttribute(String name, Object value)
+        // value 객체를 name 이름으로 추가 
+        model.addAttribute("list" , a);
+
+        return "";
+    }*/
+
+
+    /*    @RequestMapping("detail")
+    public String detail( int no, Model model) {
+
+        Flw mno = flwService.get(no);
+        model.addAttribute("mno" , mno);
+
+
+        return "flwlist";
+    }
+
+    @RequestMapping("delete")
+    public String delete(int no) {
+
+        flwService.delete(no);
+
+
+        return "";
+    }
+     */
+
+}
