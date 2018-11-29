dbcd0f98974f63ab22864139a7ac7bf653bc1c78
lee <injin33@naver.com>
2018-11-29 18:25:21 +0900

    장면리뷰 댓글 수정 기능

--- src/main/java/bitcamp/java110/cms/dao/SceneReviewDao.java
+++ src/main/java/bitcamp/java110/cms/dao/SceneReviewDao.java
@@ -17,6 +17,7 @@ public interface SceneReviewDao {
   Integer getTotalCmtCnt(int srno);
   List<SceneReviewCmt> listCmt(Map<String, Object> condition);
   Integer deleteCmt(int cmno);
+  Integer updateCmt(SceneReviewCmt sceneReviewCmt);
   List<SceneReview> listTopSr();
   Integer addToSrAlbum(Map<String, Object> condition);
   void signOut1(int mno);
--- src/main/java/bitcamp/java110/cms/service/SceneReviewService.java
+++ src/main/java/bitcamp/java110/cms/service/SceneReviewService.java
@@ -15,6 +15,7 @@ public interface SceneReviewService {
   int getTotalCmtCnt(int srno);
   List<SceneReviewCmt> listCmt(int srno, Paging paging);
   void deleteCmt(int cmno);
+  void updateCmt(SceneReviewCmt sceneReviewCmt);
   List<SceneReview> listTopSr();
   void addToSrAlbum(int lbmno, int srno);
   SceneReview initSceneReview(MovieDb tmdbMovie, SceneReview sceneReview);
