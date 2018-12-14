a5496fac6af45be6769b728c125cd5e0c8f409f4
JeahaOh <jeaha1217@gmail.com>
2018-12-14 21:03:51 +0900

    .

--- src/main/webapp/js/top-btn.js
+++ src/main/webapp/js/top-btn.js
@@ -1,15 +1,9 @@
 $(function(){
   $(window).scroll(function () {
     var endPoint = $(document).height() - $('footer').height() - $('header').height();
-    console.log( 'document.height : ' + $(document).height() );
-    console.log( 'header.height : ' + $('header').height() );
-    console.log( 'footer.height : ' + $('footer').height() );
-    console.log( 'endPoint: ' + endPoint );
-
 
     if ($(this).scrollTop() >= 100) {
       $('#top').fadeIn();
-      
       
       if(endPoint - 850 < $(this).scrollTop()){
         $('#top').css('bottom', 10 + $('footer').height() );
@@ -17,20 +11,15 @@ $(function(){
         $('#top').css('bottom','15px');
       }
       
-      
     } else {
       $('#top').fadeOut();
     }
   });
-  
   
   $('#top').click(function() {
     $('body,html').animate({
       scrollTop : 0
     }, 500);
   });
-  
-  
-  
   
 }); 
